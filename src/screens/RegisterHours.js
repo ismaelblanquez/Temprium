55dcbe71c85118b686bc9553f6d6367edb6963a6
ismaelblanquez <ismaelblanquez@gmail.com>
2023-05-20 14:26:36 +0200

    20/05/23
    Cambio de estilos

--- src/screens/RegisterHours.js
+++ src/screens/RegisterHours.js
@@ -168,8 +168,8 @@ const styles = StyleSheet.create({
   },
   headerContainer: {
     backgroundColor: '#E1F5FE',
-    borderRadius: 12,
-    borderWidth: 4,
+    // borderRadius: 12,
+    // borderWidth: 4,
     borderColor: '#0096C7',
     width: '80%',
     marginLeft: '9%',
@@ -181,7 +181,7 @@ const styles = StyleSheet.create({
     padding: '4%'
   },
   titulo: {
-    fontSize: 20,
+    fontSize: 30,
     fontWeight: 'bold',
     color: '#0096C7',
   },
