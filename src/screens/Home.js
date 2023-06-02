9c0d0c9e46d40a2c6a41ac6eb71720e49b57b45c
alejandrovillamor <122881552+alejandrovillamor@users.noreply.github.com>
2023-06-02 12:27:11 +0200

    Update home.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -257,8 +257,8 @@ const styles = StyleSheet.create({
     },
     headerContainer: {
         backgroundColor: '#E1F5FE',
-        borderRadius: 12,
-        borderWidth: 4,
+        // borderRadius: 12,
+        // borderWidth: 4,
         borderColor: '#0096C7',
         width: '80%',
         marginLeft: '9%',
@@ -269,7 +269,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     horasTotalesTitulo: {
-        fontSize: 16,
+        fontSize: 20,
         fontWeight: 'bold',
         color: '#0096C7',
     },
@@ -281,7 +281,7 @@ const styles = StyleSheet.create({
         // backgroundColor:'black'
     },
     horasTotalesNumero: {
-        fontSize: 24,
+        fontSize: 30,
         fontWeight: 'bold',
         color: '#0096C7',
     },
