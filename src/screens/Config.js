c676d55c80ca2a63bfeea275a59561c38fa85e18
alejandrovillamor <122881552+alejandrovillamor@users.noreply.github.com>
2023-06-05 08:37:33 +0200

    Update Config.js

--- src/screens/Config.js
+++ src/screens/Config.js
@@ -68,8 +68,7 @@ const Config = ({ navigation }) => {
                     Alert.alert("En mantenimiento");
                     break;
                 case 3:
-                    // navigation.navigate('privacy');
-                    Alert.alert("En mantenimiento");
+                    navigation.navigate('Privacy');
                     break;
                 case 4:
                     //navigation.navigate('Security');
